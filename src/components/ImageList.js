4bb9621c085c90b7c66199983588671106395461
belcortes <belcortes@gmail.com>
2019-01-07 11:51:52 -0800

    adding data to frontend

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import data from '../assets/data/dogs.json'
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+
+// Importing local data
+import data from '../data/dogs.json'
 
 const ImageList = () => (
   <div>
     {console.log(data)}
+    <GridList>
+      {data.dogs.map(dog => (
+        <GridListTile key={dog.image} cols={dog.cols || 1}>
+          <img src={dog.image} alt={dog.source}/>
+        </GridListTile>
+      ))}
+    </GridList>
   </div>
 );
 

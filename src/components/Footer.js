528740699f5650e339a336702ea8940dac4d9670
belcortes <belcortes@gmail.com>
2019-01-07 11:30:32 -0800

    first commit - setting up app

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="site-footer">
+    <p>Built by a wonderful developer for Asana.</p>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/ImageList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import data from '../assets/data/dogs.json'
+
+const ImageList = () => (
+  <div>
+    {console.log(data)}
+  </div>
+);
+
+export default ImageList;
